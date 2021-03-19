7af4ef0a81d5c09f19552d1bb0e82c63d3f50396
HM <hieumdd@gmail.com>
2021-03-19 18:47:16 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,71 @@
+import os
+from datetime import datetime, timedelta
+
+import requests
+from google.cloud import bigquery
+
+
+class FBOfflineConversionJob:
+    def __init__(self, day=7):
+        self.OFFLINE_EVENT_SET_ID = os.getenv("OFFLINE_EVENT_SET_ID")
+        self.API_VER = os.getenv("API_VER")
+        self.fetch_date = (datetime.now() - timedelta(days=day)).strftime("%Y-%m-%d")
+
+    def fetch_data(self):
+        client = bigquery.Client()
+        rows = client.query(
+            f"""SELECT
+            UNIX_SECONDS(TIMESTAMP_ADD(TIMESTAMP(TRANDATE), INTERVAL 990 MINUTE)) AS TRANDATE,
+            TO_HEX(SHA256(CUSTOMER_PHONE)) AS CUSTOMER_PHONE,
+            TRANID,
+            CAST(NET_AMOUNT AS INT64) AS NET_AMOUNT
+            FROM NetSuite.SalesOrder
+            WHERE TRANDATE = '{self.fetch_date}'
+            AND CUSTOMER_PHONE IS NOT NULL
+            """
+        ).result()
+        client.close()
+        return [dict(zip(row.keys(), row.values())) for row in rows]
+
+    def push(self, rows):
+        mapper = lambda x: {
+            "match_keys": {
+                "phone": [x["CUSTOMER_PHONE"]]
+            },
+            "currency": "VND",
+            "value": x["NET_AMOUNT"],
+            "event_name": "Purchase",
+            "event_time": x["TRANDATE"],
+            "order_id": x["TRANID"],
+            "custom_data": {"event_source": "in_store"},
+        }
+        rows = list(map(mapper, rows))
+        
+        with requests.post(
+            "https://graph.facebook.com/{API_VER}/{OFFLINE_EVENT_SET_ID}/events".format(
+                API_VER=os.getenv("API_VER"),
+                OFFLINE_EVENT_SET_ID=os.getenv("OFFLINE_EVENT_SET_ID"),
+            ),
+            params={"access_token": os.getenv("ACCESS_TOKEN")},
+            json={"upload_tag": "store_data", "data": rows},
+        ) as r:
+            try:
+                r.raise_for_status()
+            except:
+                pass
+            finally:
+                res = r.json()
+        return res
+
+    def run(self):
+        rows = self.fetch_data()
+        results = self.push(rows)
+        print(results)
+
+
+def main(request):
+    vuanem_fb_offline_conversion = FBOfflineConversionJob()
+    vuanem_fb_offline_conversion.run()
+
+
+main(0)
